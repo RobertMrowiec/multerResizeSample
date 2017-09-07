ba664a7955e42ebadf7a487ed139f9c9aed10a30
Marcin Wydrzyński <wydrzynskimarcin@gmail.com>
2017-09-07 18:12:50 +0200

    changes

--- index.js
+++ index.js
@@ -11,12 +11,25 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname+'/index.html')
 })
 
-app.post('/upload', multer({ dest: './uploads/' }).single('file'), (req, res) => {
 
-    Jimp.read(__dirname + "/" + req.file.path).then(function (lenna) {
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, __dirname + '/uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage }).single('file')
+
+
+app.post('/upload', upload, (req, res) => {
+
+    Jimp.read(req.file.path).then(function (lenna) {
         if (req.file.originalname.match(/.+\.(jpg|png)$/)) {
             lenna.resize(10, 10)
-                .write(__dirname + '/uploads/' + req.file.originalname)
+                .write(__dirname + '/uploadsMini/' + req.file.originalname)
         }
     }).catch(function (err) {
         console.error(err)
